8c2ab38e554fbfd88b5432424d7a4921e1b86742
whjrdemostudent <95561740+whjrdemostudent@users.noreply.github.com>
2022-04-28 19:52:02 -0400

    Add files via upload

---
+++ main.js
@@ -0,0 +1,43 @@
+noseX = 0;
+noseY = 0;
+difference = 0;
+leftWristX = 0;
+rightWristX = 0;
+
+function setup() {
+    video = createCapture(VIDEO);
+    video.size(550, 650);
+
+    canvas = createCanvas(550, 500);
+    canvas.position(600, 100);
+
+    poseNet = ml5.poseNet(video, modelLoaded);
+    poseNet.on('pose', gotPoses);
+}
+
+function gotPoses(results) {
+    if (results.length > 0) {
+        console.log(results);
+
+        noseX = results[0].pose.nose.x;
+        noseY = results[0].pose.nose.y;
+        console.log("noseX = " + noseX + "noseY = " + noseY);
+
+        leftWristX = results[0].pose.leftWrist.x;
+        rightWristX = results[0].pose.rightWrist.x;
+        difference = floor(leftWristX - rightWristX);
+
+        console.log("leftWristX = " + leftWristX + "rightWristX = " + rightWristX + "difference  = " + difference);
+    }
+}
+
+function draw() {
+    background('red')
+    fill("orange")
+    textSize(difference)
+    text("Aditya", noseX, noseY)
+}
+
+function modelLoaded() {
+    console.log("PoseNet is initialized")
+}
